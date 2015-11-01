dd187b80e657b2cf2ce666b47994d1def7965f70
sigrunio <sigruno11@ru.is>
2015-11-01 16:50:11 +0000

    Revert hello to chuck+hello

--- src/main/java/is/ru/arnlaugsson/chuck_joke/ChuckWeb.java
+++ src/main/java/is/ru/arnlaugsson/chuck_joke/ChuckWeb.java
@@ -5,10 +5,18 @@ import static spark.Spark.*;
 import spark.servlet.SparkApplication;
 
 
-public class ChuckWeb {
+public class ChuckWeb implements SparkApplication {
 
     public static void main(String[] args) {
 
-        get("/hello", (req, res) -> "Hello World");
-    }
+        staticFileLocation("/public");
+        SparkApplication chuckweb = new ChuckWeb();
+
+        String port = System.getenv("PORT");
+        if (port != null) {
+            port(Integer.valueOf(port));
+        }
+
+        post("/hello", (req, res) -> "Hello World");
+
 }
